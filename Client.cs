4f0ecfa9c39bb5a6a9060b670c564843a0593815
holly <h.person.notrobot@gmail.com>
2024-05-09 20:05:15 +0100

    Revert "Added port support"
    This reverts commit bbb8435ef027f3555a290e208ab0516d9b640eeb.

--- Client.cs
+++ Client.cs
@@ -1,18 +1,6 @@
-using System.Text;
-using System.Net;
-using System.Net.Sockets;
+namespace ConsoleBattleships;
 
-
-
-namespace ConsoleBattleships
+public class Client
 {
-    public class Client
-    {
-        int port = 21002;
-        
-        public Client(int port = 21002)
-        {
-            this.port = port;
-        }
-    }
+    
 }
